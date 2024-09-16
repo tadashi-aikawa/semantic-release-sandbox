1e74b0fe27c4bf61c05465e756955b49e2ef756d
tadashi-aikawa <syou.maman@gmail.com>
2024-09-16 21:37:36 +0900

    ci: add changelog plugin

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -6,20 +6,26 @@ export default {
       {
         preset: "conventionalcommits",
         releaseRules: [
-          { breaking: true, release: "minor" }, // TODO: v1になったら release: "major" にする
+          { breaking: true, release: "major" }, // TODO: v1になったら release: "major" にする
           { type: "feat", section: "Features", release: "minor" },
           { type: "fix", section: "Bug Fixes", release: "patch" },
           { revert: true, release: "patch" },
         ],
       },
     ],
-    // [
-    //   "@semantic-release/release-notes-generator",
-    //   {
-    //     "preset": "conventionalcommits"
-    //   }
-    // ],
-    // "@semantic-release/github",
-    // "@semantic-release/git"
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+      },
+    ],
   ],
 };
